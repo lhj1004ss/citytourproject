ce8fbd344e98a58bea458e9e745010a340981dfc
hyojelee <hyojelee1004ss@gmail.com>
2020-01-23 12:15:39 -0500

    tour project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar>hehe</Navbar>
+        <TourList></TourList>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
